e496e22b324b33979a5f3f5968b0913951c5de13
GillesRitchmond <gritchmond@gmail.com>
2021-09-23 20:40:21 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,203 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>SHANA FOR THE KIDS</title>
+
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- CUSTOM CSS -->
+    <link rel="stylesheet" href="style/style.css">
+
+    <!-- BOOTSTRAP CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+
+    <!-- BOOTSTRAP JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-white sticky-top">
+        <div class="container">
+            <a class="navbar-brand" href="index.php">
+                <img src="images/logo.png" alt="" width="auto" height="50" class="d-inline-block align-text-top">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="#aboutus">ABOUT US</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#thefounder">THE FOUNDER</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#projects">PROJECTS</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#activities">ACTIVITIES</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link donate" href="#donate">DONATE</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="line"></div>
+    </nav>
+
+
+    <div class="container content">
+        <div class="row" id="aboutus">
+            <div class="col">
+                <div class="row">
+                    <div class="col">
+                        <span class="title">About us</span>
+
+                        <div class="pink-title">SHANA FOR THE KIDS,</div>
+
+                        <p> a non-profit organization dedicated to the health, well-being, education, and housing for impoverished
+                            children currently residing in Haiti's island nation with plans to expand to other Caribbean countries
+                            as well as other third world regions of the globe. </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col">
+                <img src="images/banner.png" alt="" width="auto" height="400" class="d-inline-block align-text-top">
+            </div>
+        </div>
+
+        <br>
+        <br>
+        <br>
+
+        <div id="thefounder">
+            <div class="row">
+                <div class="col">
+                    <img src="images/founder.png" alt="" width="auto" height="400" class="d-inline-block align-text-top">
+                </div>
+
+                <div class="col">
+                    <div class="row">
+                        <div class="col">
+                            <span class="title">THE FOUNDER</span>
+
+                            <div class="pink-title">Shanaelle Sommervil,</div>
+
+                            <p>
+                                currently resides in West Orange New Jersey, currently working towards her bachelor’s degree in psychology
+                                at Montclair State University. She immigrated to the United States at a young age, where she thrived scholastically
+                                but never lost sight of her how blessed and how many people like her are not afforded the same opportunism she is.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col">
+                    <div class="row">
+                        <div class="col">
+                            <br>
+                            <p>
+                                Haiti is one of, if not the poorest and most desperate nation in the world, with two thirds of the nation’s populace
+                                living off just 2 dollars (USD) and 25 percent of the people living in extreme poverty basic survival is almost at premium in Haiti.
+                                While visiting her native land, Shanaelle experienced first hand the tremendous poverty and despair that has consumed the nation.
+                                Most notably the children were the most affected. Nearly 100,000 children of Haiti under the age of 5 suffer from acute malnutrition.
+                                This coupled with over 30 % of population that is considered by the United Nations as “Food Insecure”. The poverty problem is only
+                                magnified by the stark lack of education currently available. Haiti’s 60.5 percent literacy rate is among the lowest in the world,
+                                incompared to the 92 percent literacy rate enjoyed by other Caribbean and Latin American nations this is an embarrassment to the
+                                hemisphere.
+
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col">
+                    <img src="images/next_1.png" alt="" width="auto" height="100" class="d-inline-block align-text-top">
+                    <br><br><br>
+                    <img src="images/child-about-us-1.png" alt="" width="auto" height="350" class="d-inline-block align-text-top">
+                </div>
+            </div>
+
+            <div class="row">
+
+                <div class="col">
+                    <img src="images/next_2.png" alt="" width="auto" height="100" class="d-inline-block align-text-top">
+                    <br>
+                    <img src="images/child-about-us.png" alt="" width="auto" height="350" class="d-inline-block align-text-top">
+                </div>
+
+                <div class="col">
+                    <div class="row">
+                        <div class="col">
+
+                            <p>
+                                During her “vacation” Shanaelle felt a pull to “Do-something” for her children of Haiti. Coincidently she met Fransnel Dorsainvil
+                                who was a member of the organization PAC-Haiti. Shanaelle became an Ambassador for the organization and began to participate in
+                                several events and activities sponsored by PAC-Haiti. This included fundraising, outreach, distribution of food, medicine,
+                                and school supplies to the most at-risk members of the community. The education Shanaelle received working with the PAC-Haiti
+                                has prepared her to begin sowing the seeds for her own organization that will more directly display her vision of how an organization
+                                could be most effective in curtailing some of the hardships impoverished children face.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <br><br>
+
+    <div class="section-project" id="projects">
+        <span class="white-title centered">PROJECTS</span>
+
+        <div class="mt-5 p-3" data-rate="0.5" data-effect="slide-up">
+                <div class="xp-list mt-5" >
+                    <h5>Project 1</h5>
+                    <div class="xp" data-rate="0.5" data-effect="slide-right">
+                        In July 2017, We visited a community called Despuzeau where a feeding program was available for the most vulnerable children. I was honored to serve 
+                        the children, share with them and spend some quality time with them.
+                        We serve  50-100 kids.
+                    </div>
+                </div>
+
+                <div class="xp-list mt-5" data-rate="0.5" data-effect="slide-left">
+                    <h5>Project 2</h5>
+                    <div class="xp">
+                        In August 2018, We went to the same community. where we had a Psyco-educative day for the kids. We spent the day with them, we eat, drink, play with them, 
+                        and we also had some People who did some sessions for them. For example, we had a group of Nurse, who had a presentation about Adolescent Sexual Behavior for 
+                        the teens, we had a dentist who had a presentation about hygiene, A teacher who had a presentation about Importance of education, and a Social worker/secretary 
+                        who had a presentation about Self-esteem. At the end of the day, Shana who came from USA had a little surprise for the kids. The surprise was an Hygiene 
+                        kit(Toothbrush,Toothpaste,Soap). We serve 150-170 kids. 
+                    </div>
+                </div>
+
+                <div class="xp-list mt-5">
+                    <h5>Project 3</h5>
+                    <div class="xp">
+                        We are currently kicking off our fund drive for our summer mission to the Notre Dame Du Monte' Carmel  Orphanage located  in Saut- D’eau, Haiti in 2018 we were 
+                        able to distribute school and medical supplies and interact with the many children who call the orphanage home.
+                        <br><br>
+                        The SFTK team will be visiting our sponsored orphanage between starting on August 9th and August 11th. We will have  with the main event occurring on  Saturday, August 10th.
+                        <br><br>
+                        During the event, we will not only offer fun and educational games, distribute food, but most importantly this year we will introduce SCHOLAR KITS (BACKPACKS, NOTEBOOKS, PEN, 
+                        PENCILS, ERASER, etc), all for approximately 250 kids.
+                    </div>
+                </div>
+            </div>
+            
+
+    </div>
+
+    <br><br>
+
+</body>
+
+</html>
